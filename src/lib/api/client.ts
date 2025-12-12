72b6cd507aca4b63835d6b53b4c6640826bf9825
estanexanavsem <zvfytmbf77jbs0@hotmail.com>
2025-12-13 00:48:16 +0300

    chore: add debug log to axios client

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -4,6 +4,8 @@ import { env } from "@/env";
 
 const TIMEOUT = 5 * 60 * 1000; // 5 minutes
 
+console.log(">>>>>>>>> AXIOS CLIENT <<<<<<<<<<", { env: env.NEXT_PUBLIC_API_BASE_URL });
+
 export const apiClient: AxiosInstance = axios.create({
   baseURL: env.NEXT_PUBLIC_API_BASE_URL,
   timeout: TIMEOUT,
